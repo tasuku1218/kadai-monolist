ccd1f58d70a4b2de9c5c13f3f22c2cd99267bc7d
t.c <s03210462000@yahoo.co.jp>
2019-05-26 16:51:04 +0000

    want

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,9 @@ class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:show]
   
   def show
-    @user = current_user
+    @user = User.find(params[:id])
+    @items = @user.items.uniq
+    @count_want = @user.want_items.count
   end
 
   def new
