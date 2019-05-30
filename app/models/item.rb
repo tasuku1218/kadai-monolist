9947a665df93e5708015a258f5db5e26f9555286
t.c <s03210462000@yahoo.co.jp>
2019-05-30 00:59:51 +0000

    HAVE

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,9 +4,12 @@ class Item < ApplicationRecord
   validates :url, presence: true, length: { maximum: 255 }
   validates :image_url, presence: true, length: { maximum: 255 }
   
+  
   has_many :ownerships
   has_many :users, through: :ownerships
   has_many :wants
   has_many :want_users, through: :wants, source: :user
+  has_many :haves, class_name: 'Have'
+  has_many :have_users, through: :haves, source: :user
   
 end
